e98f5368eae593af1ac06aa60a3d86a80334fea1
yunseong7 <yunseong7@naver.com>
2022-02-20 13:24:23 +0900

    Vue1일차

---
+++ Vuejs/vue13인스턴스속성/배열은 객체다.js
@@ -0,0 +1,11 @@
+var arr = [];
+arr['0'] = 1;
+arr['1'] = 2;
+console.log(arr);
+console.log(arr['0']);
+
+var arr = {};
+arr['0'] = 1;
+arr['1'] = 2;
+console.log(arr);
+console.log(arr['0']);
---
+++ Vuejs/vue17템플릿/sfcprj01/src/App.vue
@@ -0,0 +1,22 @@
+<style scoped>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
+
+<template>
+    <div id="app">
+        <p>sejoong</p>
+    </div>
+</template>
+
+<script>
+export default {
+    components: {}
+};
+</script>
